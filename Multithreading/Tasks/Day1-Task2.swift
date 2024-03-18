b14b10caf12e121eab436dfb9ea1817eccb54e4e
Dmitry Lorents <zmey.task@gmail.com>
2024-03-18 23:17:51 +0300

    Finish task

--- Multithreading/Tasks/Day1-Task2.swift
+++ Multithreading/Tasks/Day1-Task2.swift
@@ -10,20 +10,46 @@ import UIKit
 class Day1_Task2: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            // Создаем и запускаем поток с таймером
+            let timer = TimerThread(duration: 10)
+            timer.start()
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    class TimerThread: Thread {
+        private var timerDuration: Int
+        private var timer: Timer!
+        
+        init(duration: Int) {
+            self.timerDuration = duration
+        }
+        
+        override func main() {
+            // Создаем таймер, который будет выполняться каждую секунду
+            timer = Timer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            
+            print(Thread.current)
+            
+            // Добавляем таймер в текущий run loop ниже
+            RunLoop.current.add(timer, forMode: .default)
+            
+            // Запускаем текущий run loop ниже
+            RunLoop.current.run()
+        }
+        
+        @objc func updateTimer() {
+            // Ваш код здесь будет выполняться каждую секунду
+            if timerDuration > 0 {
+                print("Осталось \(timerDuration) секунд...")
+                timerDuration -= 1
+            } else {
+                print("Время истекло!")
+                
+                timer.invalidate()
+                // Остановка текущего run loop после завершения таймера
+                CFRunLoopStop(CFRunLoopGetCurrent())
+            }
+        }
     }
-    */
-
 }
