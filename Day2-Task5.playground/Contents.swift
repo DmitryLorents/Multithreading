ff0af377a005466bf5071cb44eafbf85cbad7f02
Dmitry Lorents <zmey.task@gmail.com>
2024-03-20 11:46:34 +0300

    Finish task 6

---
+++ Day2-Task5.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class ViewController: UIViewController {
+     override func viewDidLoad() {
+              super.viewDidLoad()
+              print(2)
+              DispatchQueue.main.async {
+                      print(3)
+                      DispatchQueue.main.sync {
+                            print(5)
+                      }
+               print(4)
+             }
+           print(6)
+     }
+}
+
+let vc = ViewController()
+print(1)
+let view = vc.view
+print(7)
+
+// ViewDidLoad вызвался потому, что мы обратились к свойству view у инициализированного контроллера
