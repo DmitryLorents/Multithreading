f65b62e03fb9925f8b5e22b6ac0bdd093b8d27de
Dmitry Lorents <zmey.task@gmail.com>
2024-03-22 11:19:20 +0300

    Add Task4

--- Multithreading/SceneDelegate.swift
+++ Multithreading/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = Task3Day4()
+        window?.rootViewController = Task4Day4()
         window?.makeKeyAndVisible()
     }
 
