57a6cb373d21e414bf815020fd2e6cb52724b878
Dmitry Lorents <zmey.task@gmail.com>
2024-03-22 10:49:32 +0300

    Finish Task2

---
+++ Multithreading/Tasks/Day4/Task2Day4.swift
@@ -0,0 +1,77 @@
+//
+//  Task2Day4.swift
+//  Multithreading
+//
+//  Created by Dmitry on 22.03.2024.
+//
+
+import UIKit
+
+protocol RMOperationProtocol {
+    // Приоритеты
+    var priority: DispatchQoS.QoSClass { get }
+    // Выполняемый блок
+    var completionBlock: (() -> Void)? { get }
+    // Завершена ли операция
+    var isFinished: Bool { get }
+    // Метод для запуска операции
+    func start()
+}
+
+class RMOperation: RMOperationProtocol {
+    var priority: DispatchQoS.QoSClass
+    
+    var completionBlock: (() -> Void)?
+    
+    var isFinished: Bool = false
+    
+    init(priority: DispatchQoS.QoSClass = .unspecified, completionBlock: ( () -> Void)? = nil) {
+        self.priority = priority
+        self.completionBlock = completionBlock
+    }
+    
+    func start() {
+        let globalQueue = DispatchQueue.global(qos: priority)
+        globalQueue.async {
+            self.completionBlock?()
+        }
+    }
+    /// В методе start. реализуйте все через глобальную паралельную очередь с приоритетами.
+}
+
+class Task2Day4: UIViewController {
+    
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        view.backgroundColor = .cyan
+           
+            let operationFirst = RMOperation()
+            let operationSecond = RMOperation()
+            
+           
+            operationFirst.priority = .userInitiated
+            operationFirst.completionBlock = {
+                
+                for _ in 0..<50 {
+                    print(2)
+                }
+                print(Thread.current)
+                print("Операция полностью завершена!")
+            }
+           
+            operationFirst.start()
+            
+            operationSecond.priority = .background
+            operationSecond.completionBlock = {
+              
+                for _ in 0..<50 {
+                    print(1)
+                }
+                print(Thread.current)
+                print("Операция полностью завершена!")
+            }
+            operationSecond.start()
+
+        }
+}
