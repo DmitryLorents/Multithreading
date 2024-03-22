a9a8edf6835eb5b1833324267544558c3d051aa6
Dmitry Lorents <zmey.task@gmail.com>
2024-03-22 23:47:04 +0300

    Fix method

--- Multithreading/Tasks/Day4/Task4Day4.swift
+++ Multithreading/Tasks/Day4/Task4Day4.swift
@@ -8,77 +8,72 @@
 import UIKit
 
 class Task4Day4: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-           
-            // Использование
-            let threadSafeArray = ThreadSafeArray()
-            let operationQueue = OperationQueue()
-
-            let firstOperation = FirstOperation(threadSafeArray: threadSafeArray)
-            let secondOperation = SecondOperation(threadSafeArray: threadSafeArray)
-
-            operationQueue.addOperation(firstOperation)
-            operationQueue.addOperation(secondOperation)
-
-            // Дождитесь завершения операций перед выводом содержимого массива
-            operationQueue.waitUntilAllOperationsAreFinished()
-        Task {
-            print(await threadSafeArray.getAll())
-        }
-
-            
-        }
-
+        
+        // Использование
+        let threadSafeArray = ThreadSafeArray()
+        let operationQueue = OperationQueue()
+        
+        let firstOperation = FirstOperation(threadSafeArray: threadSafeArray)
+        let secondOperation = SecondOperation(threadSafeArray: threadSafeArray)
+        secondOperation.addDependency(firstOperation)
+        operationQueue.addOperation(firstOperation)
+        operationQueue.addOperation(secondOperation)
+        
+        // Дождитесь завершения операций перед выводом содержимого массива
+        operationQueue.waitUntilAllOperationsAreFinished()
+        print(threadSafeArray.getAll())
+        
+        
+    }
+    
 }
 
-    // Объявляем класс для для синхронизации потоков
+// Объявляем класс для для синхронизации потоков
 
-    actor ThreadSafeArray {
-        private var array: [String] = []
-
-        func append(_ item: String) {
-            array.append(item)
-        }
-
-        func getAll() -> [String] {
-            return array
-        }
+class ThreadSafeArray {
+    private var array: [String] = []
+    
+    func append(_ item: String) {
+        array.append(item)
     }
-
-    // Определяем первую операцию для добавления строки в массив
-    class FirstOperation: Operation {
-        let threadSafeArray: ThreadSafeArray
-
-        init(threadSafeArray: ThreadSafeArray) {
-            self.threadSafeArray = threadSafeArray
-        }
-
-        override func main() {
-            if isCancelled { return }
-            Task {
-               await threadSafeArray.append("Первая операция")
-            }
-            
-        }
+    
+    func getAll() -> [String] {
+        return array
     }
+}
 
-    // Определяем вторую операцию для добавления строки в массив
-
-    class SecondOperation: Operation {
-        // Создаем по образу первой операции
-        let threadSafeArray: ThreadSafeArray
-
-        init(threadSafeArray: ThreadSafeArray) {
-            self.threadSafeArray = threadSafeArray
-        }
-
-        override func main() {
-            if isCancelled { return }
-            Task {
-               await threadSafeArray.append("Вторая операция")
-            }
-        }
+// Определяем первую операцию для добавления строки в массив
+class FirstOperation: Operation {
+    let threadSafeArray: ThreadSafeArray
+    
+    init(threadSafeArray: ThreadSafeArray) {
+        self.threadSafeArray = threadSafeArray
     }
+    
+    override func main() {
+        if isCancelled { return }
+        threadSafeArray.append("Первая операция")
+        
+    }
+}
+
+// Определяем вторую операцию для добавления строки в массив
+
+class SecondOperation: Operation {
+    // Создаем по образу первой операции
+    let threadSafeArray: ThreadSafeArray
+    
+    init(threadSafeArray: ThreadSafeArray) {
+        self.threadSafeArray = threadSafeArray
+    }
+    
+    override func main() {
+        if isCancelled { return }
+        threadSafeArray.append("Вторая операция")
+        
+    }
+}
