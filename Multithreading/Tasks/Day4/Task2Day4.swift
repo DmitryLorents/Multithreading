eb5702584303b1683675cc9cc91270dacdf4cd83
Dmitry Lorents <zmey.task@gmail.com>
2024-03-22 11:05:59 +0300

    Add task3

--- Multithreading/Tasks/Day4/Task2Day4.swift
+++ Multithreading/Tasks/Day4/Task2Day4.swift
@@ -14,16 +14,20 @@ protocol RMOperationProtocol {
     var completionBlock: (() -> Void)? { get }
     // Завершена ли операция
     var isFinished: Bool { get }
+    // Начата ли операция
+    var isExecuting: Bool { get }
     // Метод для запуска операции
     func start()
 }
 
 class RMOperation: RMOperationProtocol {
+    
     var priority: DispatchQoS.QoSClass
     
     var completionBlock: (() -> Void)?
     
     var isFinished: Bool = false
+    var isExecuting: Bool = false
     
     init(priority: DispatchQoS.QoSClass = .unspecified, completionBlock: ( () -> Void)? = nil) {
         self.priority = priority
@@ -32,8 +36,14 @@ class RMOperation: RMOperationProtocol {
     
     func start() {
         let globalQueue = DispatchQueue.global(qos: priority)
-        globalQueue.async {
+        let group = DispatchGroup()
+        globalQueue.async(group: group) {
+            self.isExecuting = true
             self.completionBlock?()
+        }
+        group.wait()
+        group.notify(queue: globalQueue) {
+            self.isFinished = true
         }
     }
     /// В методе start. реализуйте все через глобальную паралельную очередь с приоритетами.
@@ -41,37 +51,37 @@ class RMOperation: RMOperationProtocol {
 
 class Task2Day4: UIViewController {
     
-
+    
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         view.backgroundColor = .cyan
-           
-            let operationFirst = RMOperation()
-            let operationSecond = RMOperation()
+        
+        let operationFirst = RMOperation()
+//        let operationFirst = BlockOperation()
+        let operationSecond = RMOperation()
+        
+        operationFirst.priority = .userInitiated
+        operationFirst.completionBlock = {
             
-           
-            operationFirst.priority = .userInitiated
-            operationFirst.completionBlock = {
-                
-                for _ in 0..<50 {
-                    print(2)
-                }
-                print(Thread.current)
-                print("Операция полностью завершена!")
+            for _ in 0..<50 {
+                print(2)
             }
-           
-            operationFirst.start()
-            
-            operationSecond.priority = .background
-            operationSecond.completionBlock = {
-              
-                for _ in 0..<50 {
-                    print(1)
-                }
-                print(Thread.current)
-                print("Операция полностью завершена!")
-            }
-            operationSecond.start()
-
+            print(Thread.current)
+            print("Операция полностью завершена!")
         }
+        
+        operationFirst.start()
+        
+        operationSecond.priority = .background
+        operationSecond.completionBlock = {
+            
+            for _ in 0..<50 {
+                print(1)
+            }
+            print(Thread.current)
+            print("Операция полностью завершена!")
+        }
+        operationSecond.start()
+        
+    }
 }
---
+++ Multithreading/Tasks/Day4/Task3Day4.swift
@@ -0,0 +1,68 @@
+//
+//  Task3Day4.swift
+//  Multithreading
+//
+//  Created by Dmitry on 22.03.2024.
+//
+
+import UIKit
+
+protocol RMOperationQueueProtocol {
+    /// Тут храним пул наших операций
+    var operations: [RMOperation] { get }
+    /// Добавляем наши кастомные операции в пул operations
+    func addOperation(_ operation: RMOperation)
+    /// Запускаем следующую
+    func executeNextOperation()
+}
+
+// Класс, управляющий очередью операций
+final class RMOperationQueue: RMOperationQueueProtocol {
+    var operations: [RMOperation] = []
+
+    func addOperation(_ operation: RMOperation) {
+        operations.append(operation)
+        executeNextOperation()
+    }
+
+    func executeNextOperation() {
+        if let nextOperation = operations.first(where: { !$0.isExecuting && !$0.isFinished }) {
+            /// Тут делаем старт операции
+            nextOperation.start()
+            /// Тут рекурсивно запускаем следующую операцию (что такое рекурсия?)
+            executeNextOperation()
+        }
+    }
+}
+
+class Task3Day4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        
+        let rmOperationQueue = RMOperationQueue()
+        
+        let rmOperation1 = RMOperation()
+        rmOperation1.priority = .background
+        
+        rmOperation1.completionBlock = {
+            print(1)
+        }
+        
+        let rmOperation2 = RMOperation()
+        rmOperation2.priority = .userInteractive
+        
+        rmOperation2.completionBlock = {
+            print(2)
+        }
+        
+    
+        rmOperationQueue.addOperation(rmOperation1)
+        rmOperationQueue.addOperation(rmOperation2)
+      
+    }
+
+}
+
+
