2978e0f1431e7f482535076c164fd26b646097ae
Dmitry Lorents <zmey.task@gmail.com>
2024-03-23 21:53:33 +0300

    Finish Task6

--- Multithreading/Tasks/Day5/Task5Day5.swift
+++ Multithreading/Tasks/Day5/Task5Day5.swift
@@ -9,33 +9,60 @@ import UIKit
 
 class Task5Day5: UIViewController {
     
+    
     var networkService = NetworkService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Вариант 1 без возврата ошибки
+        //        Task {
+        //            let messages = await fetchMessagesResult()
+        //            print(messages)
+        //        }
         
+        // Вариант 2 с возвратом ошибки
         Task {
-            let messages = await fetchMessagesResult()
-            print(messages)
+            do {
+                let messages = try await fetchMessagesResult()
+                print(messages)
+            } catch {
+                print("Failed to load messages with error: \(error.localizedDescription)")
+            }
+            
+            
         }
         
-    }
-    
-    func fetchMessagesResult(completion: @escaping ([Message]) -> Void) {
-        networkService.fetchMessages { message in
-            completion(message)
-        }
-    }
-    
-    @available(*, renamed: "fetchMessagesResult()")
-    func fetchMessagesResult() async -> [Message] {
-        return await withCheckedContinuation { continuation in
-            fetchMessagesResult { messages in
-                continuation.resume(returning: messages)
+        func fetchMessagesResult(completion: @escaping ([Message]) -> Void) {
+            networkService.fetchMessages { message in
+                completion(message)
             }
         }
+        
+        @available(*, renamed: "fetchMessagesResult()")
+        // Вариант 1 без возврата ошибки
+        
+        //    func fetchMessagesResult() async -> [Message] {
+        //        return await withCheckedContinuation { continuation in
+        //            fetchMessagesResult { messages in
+        //                continuation.resume(returning: messages)
+        //            }
+        //        }
+        //    }
+        
+        // Вариант 2 с возвратом ошибки
+        func fetchMessagesResult() async throws -> [Message] {
+            return try await withCheckedThrowingContinuation { continuation in
+                fetchMessagesResult { messages in
+                    if messages.isEmpty {
+                        continuation.resume(throwing: FetchError.emptyArray)
+                    } else {
+                        continuation.resume(returning: messages)
+                    }
+                }
+            }
+        }
+        
     }
-    
 }
 
 struct Message: Decodable, Identifiable {
@@ -48,7 +75,9 @@ struct Message: Decodable, Identifiable {
 class NetworkService {
     
     func fetchMessages(completion: @escaping ([Message]) -> Void) {
-        let url = URL(string: "https://hws.dev/user-messages.json")!
+        //            let url = URL(string: "https://hws.dev/user-messages.json")!
+        // check empty array case
+        let url = URL(string: "https://hws.dev/user-messages.json2")!
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
@@ -61,5 +90,16 @@ class NetworkService {
             completion([])
         }
         .resume()
+    }
+}
+
+enum FetchError: Error {
+    case emptyArray
+    
+}
+
+extension FetchError: LocalizedError {
+    public var errorDescription: String? {
+        "Empty array"
     }
 }
