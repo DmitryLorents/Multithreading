20577aa338fb959847ff1e77a6a77b1e585499f2
Dmitry Lorents <zmey.task@gmail.com>
2024-03-25 11:41:47 +0300

    Fix task 6

--- Multithreading/Tasks/Day3/Optional/Task6Optional.swift
+++ Multithreading/Tasks/Day3/Optional/Task6Optional.swift
@@ -9,62 +9,64 @@ import UIKit
 
 class Task6Optional: UIViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         // используя DispathGroup вызвать методы в следующем порядке
         let group = DispatchGroup()
-        let serialQueue = DispatchQueue(label: "serial")
-        let fetchDataItem = DispatchWorkItem(flags: .barrier, block: {
-            self.fetchData()
-        })
         
-        serialQueue.async(group: group) {
-           group.enter()
-            fetchDataItem.perform()
-            group.leave()
+        DispatchQueue.global().async(group: group) {
+            //1) fetchData
+            group.enter()
+            self.fetchData {
+                group.leave()
+            }
             
         }
-        
-//        //1) fetchData
-//        group.enter()
-//        fetchData()
-//        group.leave()
-//        group.wait()
-//        // 2) decodeData
-//        group.enter()
-//        
-//        group.leave()
-//        
-//        // 3) modifyData
-//        group.enter()
-//        
-//        group.leave()
+        group.wait()
+        // 2) decodeData
+        group.enter()
+        self.decodeData {
+            group.leave()
+        }
+        group.wait()
+               
+        // 3) modifyData
+        group.enter()
+        self.modifyData {
+            group.leave()
+        }
         
         // После того как данные были получены, декодированы и обработаны вызвать метод showData()
+        group.wait()
         group.notify(queue: .main, execute: {
             self.showData()
         })
     }
     
-    func fetchData() {
+    func fetchData(_ handler: @escaping () -> ()) {
         DispatchQueue.global().async {
             sleep (10)
             print ("data did obtain")
+            handler()
         }
     }
     
-    func decodeData() {
+    func decodeData(_ handler: @escaping () -> ()) {
         DispatchQueue.global().async {
             sleep (2)
             print ("data did decode")
+            handler()
         }
     }
     
-    func modifyData() {
+    func modifyData(_ handler: @escaping () -> ()) {
         DispatchQueue.global().async {
             sleep (1)
             print ("data did modify")
+            handler()
         }
     }
     
